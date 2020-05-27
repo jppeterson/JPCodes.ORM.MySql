4d0e3b04a2b273fc5d737d476f2593f54aacf70c
Jake Peterson <atw1003p@navitus.com>
2020-05-27 17:12:48 -0500

    Initial commit

---
+++ JPCodes.ORM.MySql/MySqlDataRecord.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.Common;
+
+namespace JPCodes.ORM.MySql
+{
+    public class MySqlDataRecordAttribute : DataRecordAttribute
+    {
+        public override string ParameterPrefix => "?";
+        public override string EncloseObject(string item) => $"`{item}`";
+        public override DbParameter CreateParameter(string name, object value) => new MySqlParameter(name, value);
+    }
+}
