417398758ee7c1b208bbf23ed893fe1b6ddf3b26
Jacob Peterson <jacob.p.peterson@live.com>
2020-06-06 19:45:35 -0500

    Added constructor for tablename

--- JPCodes.ORM.MySql/MySqlDataRecord.cs
+++ JPCodes.ORM.MySql/MySqlDataRecord.cs
@@ -6,6 +6,9 @@ namespace JPCodes.ORM.MySql
 {
     public class MySqlDataRecordAttribute : DataRecordAttribute
     {
+        public MySqlDataRecordAttribute() : base() { }
+        public MySqlDataRecordAttribute(string tableName) : base(tableName) { }
+
         public override string ParameterPrefix => "?";
         public override string EncloseObject(string item) => $"`{item}`";
         public override DbParameter CreateParameter(string name, object value) => new MySqlParameter(name, value);
